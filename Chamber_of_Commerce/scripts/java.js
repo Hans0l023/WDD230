766166af0dad9525dfce2d4a1feeaba4e09e95a1
Hans0l023 <Watts.hans@gmail.com>
2022-10-15 16:45:13 -0700

    chamber updates

--- Chamber_of_Commerce/scripts/java.js
+++ Chamber_of_Commerce/scripts/java.js
@@ -11,8 +11,23 @@ fielddate.innerHTML = `<em>${fulldate}</em>`;
 function toggleMenu() {
     document.getElementById('primary_nav').classList.toggle('open');
     document.getElementById('hamburger_button').classList.toggle('open');
-
+    
 }
 
 const x = document.getElementById('hamburger_button')
 x.onclick = toggleMenu;
+
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const d = new Date();
+let day = weekday[d.getUTCDay()];
+
+
+/* MAKE SURE THIS GETS CHANGED BEFORE TURNING IN*/
+if (day === 'MOnday'){
+    greeting = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
+}else if (day === 'Tuesday'){
+        greeting = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
+}
+document.getElementById('msg4').textContent = greeting
