d7be557bc7abc487e98911ffcb67765b50901ca1
Hans0l023 <Watts.hans@gmail.com>
2022-10-28 19:05:21 -0700

    week 7 updates

--- Chamber_of_Commerce/scripts/java.js
+++ Chamber_of_Commerce/scripts/java.js
@@ -24,10 +24,42 @@ const d = new Date();
 let day = weekday[d.getUTCDay()];
 
 
-/* MAKE SURE THIS GETS CHANGED BEFORE TURNING IN*/
-if (day === 'MOnday'){
-    greeting = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
-}else if (day === 'Tuesday'){
-        greeting = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
+
+
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
+    img.src = src;
 }
-document.getElementById('msg4').textContent = greeting
+
+const img = {
+    threshold: 1,
+    rootMargin: "0px 0px -150px 0px"
+};
+
+const imgObs = new IntersectionObserver((entries,imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, img);
+
+images.forEach((image) => {
+    imgObs.observe(image);
+});
+
+
+
+if (day === 'Monday'){
+    greeting = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+}else if (day === 'Friday'){
+    greeting = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+}
