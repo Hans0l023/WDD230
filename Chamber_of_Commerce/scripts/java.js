0ab0dc6ce07e4e7d8328c42f21c511a2d0502d89
Hans0l023 <Watts.hans@gmail.com>
2022-11-10 19:35:14 -0700

    creation of kson file

--- Chamber_of_Commerce/scripts/java.js
+++ Chamber_of_Commerce/scripts/java.js
@@ -67,4 +67,46 @@ let thanks =[]
 const thanks_you = function (){
     return document.getElementById('thanks').innerHTMl = 'Thank you '+ formElement.business.value +'! For joining our Community page';
 }
-document.getElementById('button').addEventListener('click', thanks_you)
+
+const requestURL = href='data.json';
+const cards = document.querySelector('.cards');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);   
+    const companies = jsonObject['companies'];
+    companies.forEach(displaycompanies);
+});
+
+
+
+function displaycompanies(company) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let imageurl = document.createElement('img');
+    let address = document.createElement('span');
+    let phone_number = document.createElement('span');
+  
+    // Change the textContent property of the h2 element to contain the company's full name
+    h2.textContent = `${company.name} `;
+    address.textContent = `Located at: ${company.address} `;
+    phone_number.textContent= `Number: ${company.phone_number} `;
+  
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    imageurl.setAttribute('src', company.imageurl);
+    imageurl.setAttribute('alt', `${company.name}  - ${company.order}`);
+    imageurl.setAttribute('loading', 'lazy');
+  
+    // Add/append the section(card) with the h2 element
+    card.appendChild(h2);
+    card.appendChild(address);
+    card.appendChild(phone_number);
+    card.appendChild(imageurl);
+  
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+}
